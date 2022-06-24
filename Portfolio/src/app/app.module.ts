6919977f2f99ee91ad467114e990ede096b7fbad
Laureano Chavarria <LaureanoCuentas@outlook.com>
2022-06-24 16:58:55 -0300

    header terminado, falta colocar el banner

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Componente/header/header.component';
+import { LogoAPComponent } from './Componente/logo-ap/logo-ap.component';
+import { SocialesComponent } from './Componente/sociales/sociales.component';
+import { BannerComponent } from './Componente/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
